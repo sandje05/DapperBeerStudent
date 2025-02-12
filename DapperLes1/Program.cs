f670746abdc344dab9e89afd0521f71aa1a06c9b
jorislops <jorislops@gmail.com>
2025-02-12 12:15:07 +0100

    Added Les 1

---
+++ DapperLes1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Dapper;
+using MySqlConnector;
+
+Console.WriteLine("Hello, World!");
+
+string conectionString = "server=localhost;uid=root;pwd=Test@1234!;database=Todo";
+MySqlConnection connection = new MySqlConnection(conectionString);
+
+List<Todo> todos = connection.Query<Todo>("SELECT * FROM Todo").ToList();
+
+foreach (var todo in todos)
+{
+    Console.WriteLine(todo.Name + " " + todo.Completed);
+}
+
+public class Todo
+{
+    public int TodoId { get; set; }
+    public string Name { get; set; }
+    public bool Completed { get; set; } 
+}
